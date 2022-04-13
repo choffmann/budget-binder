4621815ccdcbf3139eb1ed03f2fc3b5f8a6361d2
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-04-13 14:56:14 +0200

    added the Application Flow + show the name on the Screen on the Website

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de.hsfl.budgetBinder/compose/ApplicationView.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de.hsfl.budgetBinder/compose/ApplicationView.kt
@@ -1,12 +1,21 @@
 package de.hsfl.budgetBinder.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import de.hsfl.budgetBinder.ApplicationFlow
+import de.hsfl.budgetBinder.UIState
+import de.hsfl.budgetBinder.client.Client
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 import de.hsfl.budgetBinder.model.*
+import kotlinx.coroutines.MainScope
+
 
 @Composable
 fun ApplicationView() {
+    val applicationFlow = ApplicationFlow(Client(), MainScope())
+    val uiState by applicationFlow.uiState.collectAsState(MainScope())
     Div(
         attrs = {
             classes("mdc-typography--headline4", "hello-world")
@@ -23,6 +32,11 @@ fun ApplicationView() {
                     //TODO: Style in Compose
                 }
             }
-        ) { Text("from ${Platform().platform}") }
+        ) {
+            Text("from ${Platform().platform}")
+            if(uiState is UIState.Success) {
+                Text((uiState as UIState.Success).users[0].name)
+            }
+        }
     }
 }
