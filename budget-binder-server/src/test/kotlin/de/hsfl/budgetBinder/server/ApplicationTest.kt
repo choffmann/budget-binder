6144cbb70366c000991da6c2c065235b4741ba0c
Fabian Petersen <fabian@nf-petersen.de>
2022-05-12 01:08:58 +0200

    created the TestBase for upcoming Tests

--- budget-binder-server/src/test/kotlin/de/hsfl/budgetBinder/server/ApplicationTest.kt
+++ budget-binder-server/src/test/kotlin/de/hsfl/budgetBinder/server/ApplicationTest.kt
@@ -1,20 +1,40 @@
 package de.hsfl.budgetBinder.server
-/*
+
+import de.hsfl.budgetBinder.server.config.Config
+import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-*/
+import kotlin.test.*
+
+fun <R> withCustomTestApplication(
+    moduleFunction: Application.(config: Config?, confString: String?) -> Unit,
+    test: TestApplicationEngine.() -> R
+) {
+    withApplication(createTestEnvironment()) {
+        val stringConfig = """
+            dataBase:
+                dbType: SQLITE
+                sqlitePath: file:test?mode=memory&cache=shared
+            server:
+                frontendAddresses: http://localhost:8081
+            jwt:
+                accessSecret: testSecret
+                refreshSecret: testSecret2
+                accessMinutes: 1
+            """.trimIndent()
+        moduleFunction(application, null, stringConfig)
+        test()
+    }
+}
+
 class ApplicationTest {
-    /* @Test
-    fun testRoot() = testApplication {
-        application {
-            module()
+    @Test
+    fun testRoot() {
+        withCustomTestApplication(Application::mainModule) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello, world!", response.content)
+            }
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
-    } */
+    }
 }
