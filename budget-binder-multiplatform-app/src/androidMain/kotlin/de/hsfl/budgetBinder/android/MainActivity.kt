05a8f86b71f25b823f3557d36118f3580eb3fd2d
Cedrik Hoffmann <hoffmann@geograt.de>
2022-05-23 21:20:45 +0200

    Add Prototype in Android

--- budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/MainActivity.kt
+++ budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import de.hsfl.budgetBinder.compose.App
+import de.hsfl.budgetBinder.prototyp.Prototype
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            //App()
+            Prototype()
         }
     }
 }
