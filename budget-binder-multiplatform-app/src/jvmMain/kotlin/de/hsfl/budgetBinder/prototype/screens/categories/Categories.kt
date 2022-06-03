3446bb9217bf8f87bf20ddf9877d56ad8e8fe6ed
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-03 16:49:03 +0200

    Implement clickable on Category

--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/prototype/screens/categories/Categories.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/prototype/screens/categories/Categories.kt
@@ -1,7 +1,6 @@
 package de.hsfl.budgetBinder.prototype.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
@@ -14,29 +13,10 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import de.hsfl.budgetBinder.prototype.Category
-import de.hsfl.budgetBinder.prototype.PrototypeScreen
 import de.hsfl.budgetBinder.prototype.allCategories
 
 @Composable
 fun CategoriesComponent() {
-    CategoriesListView()
+    //CategoriesListView()
 }
 
-@Composable
-fun CategoriesListView(modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(allCategories) {
-            CategoryRow(it)
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun CategoryRow(category: Category) {
-    ListItem(
-        icon = { Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = null) },
-        text = { Text(category.name) },
-        trailing = { Icon(imageVector = Icons.Filled.Check, contentDescription = null) }
-    )
-}
--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/prototype/screens/welcome/Budget.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/prototype/screens/welcome/Budget.kt
@@ -1,33 +1,51 @@
 package de.hsfl.budgetBinder.prototype.screens.welcome
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import de.hsfl.budgetBinder.prototype.screens.CategoriesListView
+import de.hsfl.budgetBinder.prototype.Category
+import de.hsfl.budgetBinder.prototype.allCategories
+import kotlinx.coroutines.launch
+
+val selectedCategories = mutableStateListOf<Category>()
 
 @Composable
 fun ChooseCategories() {
     Column(modifier = Modifier.fillMaxSize()) {
         TextHeader(modifier = Modifier.fillMaxWidth().padding(16.dp))
-        CategoriesListView(modifier = Modifier.fillMaxWidth().padding(16.dp).weight(1F))
+        CategoriesListView(modifier = Modifier.fillMaxWidth().padding(16.dp).weight(1F), onItemClick = {
+            if (!selectedCategories.contains(it)) {
+                selectedCategories.add(it)
+            } else {
+                selectedCategories.remove(it)
+            }
+
+        })
         Button(modifier = Modifier.fillMaxWidth().padding(16.dp), onClick = {}) {
-            Text(text = "Weiter")
+            Text(modifier = Modifier.padding(8.dp), text = "Weiter")
         }
     }
 }
 
 @Composable
-fun TextHeader(modifier: Modifier = Modifier) {
+private fun TextHeader(modifier: Modifier = Modifier) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
             modifier = Modifier.padding(bottom = 16.dp),
@@ -37,4 +55,24 @@ fun TextHeader(modifier: Modifier = Modifier) {
         )
         Text(text = "Wähle die Kategorie, für die du ein Budget erstellen möchtest", textAlign = TextAlign.Center)
     }
+}
+
+@Composable
+private fun CategoriesListView(modifier: Modifier = Modifier, onItemClick: (Category) -> Unit) {
+    LazyColumn(modifier = modifier) {
+        items(allCategories) {
+            CategoryRow(category = it, onItemClick = onItemClick)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun CategoryRow(category: Category, onItemClick: (Category) -> Unit) {
+    ListItem(modifier = Modifier.clickable(onClick = { onItemClick(category) }),
+        icon = { Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = null) },
+        text = { Text(category.name) },
+        trailing = {
+            if (selectedCategories.contains(category)) Icon(imageVector = Icons.Filled.Check, contentDescription = null)
+        })
 }
