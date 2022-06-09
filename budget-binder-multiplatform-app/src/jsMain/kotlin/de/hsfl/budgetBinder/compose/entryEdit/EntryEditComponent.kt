f73e3b2d22a03553e08815d5bab22f8f84ffd02f
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-09 19:00:11 +0200

    Create file structure and basic navigation for EntryEdit

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entryEdit/EntryEditComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entryEdit/EntryEditComponent.kt
@@ -1,4 +1,4 @@
-package de.hsfl.budgetBinder.compose.dashboard
+package de.hsfl.budgetBinder.compose.entryEdit
 
 import androidx.compose.runtime.*
 import de.hsfl.budgetBinder.domain.use_case.auth_user.LogoutUseCase
@@ -14,29 +14,15 @@ import org.kodein.di.compose.localDI
 import org.kodein.di.instance
 
 @Composable
-fun DashboardComponent(screenState: MutableState<Screen>) {
+fun EntryEditComponent(screenState: MutableState<Screen>) {
     val scope = CoroutineScope(Dispatchers.Unconfined + SupervisorJob())
     val di = localDI()
     val userUseCase: UserUseCase by di.instance()
-    val logoutUseCase: LogoutUseCase by di.instance()
     val userViewModel = UserViewModel(userUseCase, scope)
-    val logoutViewModel = LogoutViewModel(logoutUseCase, scope)
     val viewState = userViewModel.state.collectAsState(scope)
-    val logOutState = logoutViewModel.state.collectAsState(scope)
 
-    DashboardView(
+    EntryEditView(
         state = viewState,
-        onUpdate = { userViewModel.getMyUser() },
-        onLogout = { logoutViewModel.logOut(false) },
-        onCategorySummaryButton = { screenState.value = Screen.CategorySummary},
-        onSettingsButton = {screenState.value = Screen.Settings},
-        onEntryCreateButton = {screenState.value = Screen.EntryCreate},
-        onEntryEditButton = {screenState.value = Screen.EntryEdit}
+        onBackButton = { screenState.value = Screen.Dashboard}
     )
-
-    when (logOutState) {
-        is UiState.Success<*> -> {
-            screenState.value = Screen.Login
-        }
-    }
 }
