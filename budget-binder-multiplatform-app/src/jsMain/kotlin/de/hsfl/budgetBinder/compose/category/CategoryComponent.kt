7182e60c1846f60edc910453e426d3b6a8684bcc
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-10 22:30:56 +0200

    Add simple composable functions for data visualisation

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategoryComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategoryComponent.kt
@@ -1,12 +1,19 @@
 package de.hsfl.budgetBinder.compose.category
 
 import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.common.Category
+import de.hsfl.budgetBinder.common.Entry
 import de.hsfl.budgetBinder.domain.use_case.auth_user.LoginUseCase
 import de.hsfl.budgetBinder.presentation.LoginViewModel
 import de.hsfl.budgetBinder.presentation.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import org.jetbrains.compose.web.ExperimentalComposeWebSvgApi
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.svg.Rect
+import org.jetbrains.compose.web.svg.Svg
 import org.kodein.di.compose.localDI
 import org.kodein.di.instance
 
@@ -34,5 +41,64 @@ fun CategoryComponent(screenState: MutableState<Screen>) {
             onBackButton = { screenState.value = Screen.CategorySummary}
         )
         else -> {}
+    }
+}
+
+//Should be put in own File
+@Composable
+fun Icon(category: Category){
+    val imagePath =
+    when (category.image) {
+        Category.Image.DEFAULT -> "cart.png"
+        Category.Image.SHOPPING -> "cart.png"
+    }
+    Img(imagePath, "imagePath", attrs = {classes("mdc-icon-button")})
+}
+
+@OptIn(ExperimentalComposeWebSvgApi::class)
+@Composable
+fun Bar(category: Category, entryList: List<Entry>){
+    val width = 200
+    val height = 80
+    val budget = category.budget
+    var usedBudget = 0f
+    for (entry in entryList) {
+        usedBudget+= entry.amount
+    }
+    Div{
+        if (usedBudget < budget) {
+            //Money Text
+            Div(attrs={style {
+                display(DisplayStyle.Flex)
+                justifyContent(JustifyContent("space-between"))
+            }}){
+                Div{Text(usedBudget.toString()+"€")}
+                Div{Text(budget.toString()+"€")}
+            }
+            //Bar
+            Svg(viewBox = "0 0 200 80"){//For aspect ratio - tries to fill out wherever its in
+                Rect(x = 0, y = 0, width = width, height = height, {
+                    attr("fill", Color.lightgray.toString())
+                })
+                Rect(x = 0, y = 0, width = usedBudget/budget*width, height = height, {
+                    attr("fill", Color.darkred.toString())
+                })
+            }
+        }
+        else{
+            //SpentBudget Text
+            Div(attrs={style {
+                display(DisplayStyle.Flex)
+                justifyContent(JustifyContent("center"))
+            }}){
+                Div{Text("Budget limit for "+category.name+" reached! "+usedBudget.toString()+"€ of "+budget.toString()+"€ Budget spent")}
+            }
+            //Bar
+            Svg(viewBox = "0 0 200 80"){//For aspect ratio - tries to fill out wherever its in
+                Rect(x = 0, y = 0, width = width, height = height, {
+                    attr("fill", Color.red.toString())
+                })
+            }
+        }
     }
 }
--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entry/EntryComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entry/EntryComponent.kt
@@ -1,12 +1,15 @@
 package de.hsfl.budgetBinder.compose.entry
 
 import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.common.Entry
 import de.hsfl.budgetBinder.domain.use_case.get_user.UserUseCase
 import de.hsfl.budgetBinder.presentation.Screen
 import de.hsfl.budgetBinder.presentation.UserViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
 import org.kodein.di.compose.localDI
 import org.kodein.di.instance
 
@@ -29,5 +32,22 @@ fun EntryComponent(screenState: MutableState<Screen>) {
             onBackButton = { screenState.value = Screen.Dashboard}
         )
         else -> {}
+    }
+}
+
+//Should be put in own File
+@Composable
+fun EntryListElement(entry: Entry){
+    Div {
+        //TODO: Icon der Kategorie hier anzeigen: Category.Icon(entry.category_id)
+        Text(entry.name)
+        Text(entry.amount.toString()+"€")
+    }
+}
+
+@Composable
+fun EntryList(list: List<Entry>){
+    for (entry in list){
+        EntryListElement(entry)
     }
 }
