eb2ad82dee33c3470d2334f460ae2307be9f5da6
Dominik Heckner <dominik.heckner@stud.hs-flensburg.de>
2022-06-12 17:39:11 +0200

    Add SettingsChangeUserDataView

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/settings/SettingsComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/settings/SettingsComponent.kt
@@ -1,6 +1,8 @@
 package de.hsfl.budgetBinder.compose.settings
 
 import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.common.User
+import de.hsfl.budgetBinder.compose.settings.SettingsChangeUserDataView
 import de.hsfl.budgetBinder.domain.usecase.ChangeMyUserUseCase
 import de.hsfl.budgetBinder.domain.usecase.DeleteMyUserUseCase
 import de.hsfl.budgetBinder.presentation.Screen
@@ -17,14 +19,28 @@ fun SettingsComponent(screenState: MutableState<Screen>) {
     val di = localDI()
     val changeMyUserUseCase: ChangeMyUserUseCase by di.instance()
     val deleteMyUserUseCase: DeleteMyUserUseCase by di.instance()
-    val settingsViewModel = SettingsViewModel(changeMyUserUseCase,deleteMyUserUseCase, scope)
+    val settingsViewModel = SettingsViewModel(changeMyUserUseCase, deleteMyUserUseCase, scope)
     val viewState = settingsViewModel.state.collectAsState(scope)
 
-    SettingsView(
-        state = viewState,
-        onChangeToDashboard = { screenState.value = Screen.Dashboard} ,
-        onChangeToSettings = { screenState.value = Screen.Settings},
-        onChangeToCategory = { screenState.value = Screen.CategorySummary},
-        onDeleteButtonPressed = { settingsViewModel.deleteMyUser(); screenState.value = Screen.Login }
-    )
+    when (screenState.value) {
+        Screen.Settings -> SettingsView(
+            state = viewState,
+            onChangeToDashboard = { screenState.value = Screen.Dashboard },
+            onChangeToSettings = { screenState.value = Screen.Settings },
+            onChangeToCategory = { screenState.value = Screen.CategorySummary },
+            onDeleteButtonPressed = { settingsViewModel.deleteMyUser(); screenState.value = Screen.Login },
+            onChangeButtonPressed = { screenState.value = Screen.SettingsChangeUserData }
+        )
+        Screen.SettingsChangeUserData -> SettingsChangeUserDataView(
+            state = viewState,
+            onChangeDataButtonPressed = { firstName, lastName, password ->
+                settingsViewModel.changeMyUser(User.In(firstName, lastName, password)); screenState.value =
+                Screen.Settings
+            },
+            onChangeToDashboard = { screenState.value = Screen.Dashboard },
+            onChangeToSettings = { screenState.value = Screen.Settings },
+            onChangeToCategory = { screenState.value = Screen.CategorySummary },
+        )
+        else -> {}
+    }
 }
