d145161bdbabde5749b8bf8bd1d44cf47bc11ec5
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-12 19:16:23 +0200

    Remove Prototype

--- budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/MainActivity.kt
+++ budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/MainActivity.kt
@@ -3,15 +3,14 @@ package de.hsfl.budgetBinder.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import de.hsfl.budgetBinder.prototype.Prototype
+import de.hsfl.budgetBinder.compose.App
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            //App()
-            Prototype()
+            App()
         }
     }
 }
