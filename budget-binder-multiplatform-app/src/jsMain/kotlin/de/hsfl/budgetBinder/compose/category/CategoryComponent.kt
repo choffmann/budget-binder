d8520aa48e79c2c6cedeae6e6d7d1ec41f74d335
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-13 21:37:10 +0200

    Add all cases to BudgetBar

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategoryComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategoryComponent.kt
@@ -10,6 +10,7 @@ import de.hsfl.budgetBinder.presentation.viewmodel.CategoryViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.serialization.json.JsonNull.content
 import org.jetbrains.compose.web.ExperimentalComposeWebSvgApi
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
@@ -28,34 +29,44 @@ fun CategoryComponent(screenState: MutableState<Screen>) {
     val changeCategoryByIdUseCase: ChangeCategoryByIdUseCase by di.instance()
     val deleteCategoryByIdUseCase: DeleteCategoryByIdUseCase by di.instance()
     val getAllEntriesByCategoryUseCase: GetAllEntriesByCategoryUseCase by di.instance()
-    val categoryViewModel = CategoryViewModel(getAllCategoriesUseCase, getCategoryByIdUseCase,createCategoryUseCase, changeCategoryByIdUseCase, deleteCategoryByIdUseCase, getAllEntriesByCategoryUseCase, scope)
+    val categoryViewModel = CategoryViewModel(
+        getAllCategoriesUseCase,
+        getCategoryByIdUseCase,
+        createCategoryUseCase,
+        changeCategoryByIdUseCase,
+        deleteCategoryByIdUseCase,
+        getAllEntriesByCategoryUseCase,
+        scope
+    )
     val viewState = categoryViewModel.state.collectAsState(scope)
 
     when (screenState.value) {
         Screen.CategoryCreate -> CategoryCreateView(
             state = viewState,
-            onBackButton = { screenState.value = Screen.CategorySummary}
+            onBackButton = { screenState.value = Screen.CategorySummary }
         )
         Screen.CategorySummary -> CategorySummaryView(
             state = viewState,
-            onBackButton = { screenState.value = Screen.Dashboard},
-            onEditButton = { screenState.value = Screen.CategoryEdit},
-            onCategoryCreateButton = { screenState.value = Screen.CategoryCreate},
+            onBackButton = { screenState.value = Screen.Dashboard },
+            onEditButton = { screenState.value = Screen.CategoryEdit },
+            onCategoryCreateButton = { screenState.value = Screen.CategoryCreate },
         )
         Screen.CategoryEdit -> CategoryEditView(
             state = viewState,
-            onBackButton = { screenState.value = Screen.CategorySummary}
+            onBackButton = { screenState.value = Screen.CategorySummary }
         )
         Screen.CategoryCreateOnRegister -> CategoryCreateOnRegisterView(
             state = viewState,
-            onFinishedButton = { screenState.value = Screen.Dashboard} //Should go back to the previous Screen, which could be CategorySummary or EntryCreate.
+            onFinishedButton = {
+                screenState.value = Screen.Dashboard
+            } //Should go back to the previous Screen, which could be CategorySummary or EntryCreate.
         )
         else -> {}
     }
 }
 
-fun categoryIdToCategory(category_id: Int?,categoryList: List<Category>): Category {
-    for (category in categoryList){
+fun categoryIdToCategory(category_id: Int?, categoryList: List<Category>): Category {
+    for (category in categoryList) {
         if (category.id == category_id) return category
     }
     return DEFAULT_CATEGORY //If the category wasn't found (or is set to no category) return default
@@ -63,7 +74,7 @@ fun categoryIdToCategory(category_id: Int?,categoryList: List<Category>): Catego
 
 @OptIn(ExperimentalComposeWebSvgApi::class)
 @Composable
-fun BudgetBar(category: Category, entryList: List<Entry>){
+fun BudgetBar(category: Category, entryList: List<Entry>) {
     //category = Category we want to show
     //entryList = List of entries
     //width and height are for aspect ratio - tries to fill out wherever its in, so its more like
@@ -72,44 +83,56 @@ fun BudgetBar(category: Category, entryList: List<Entry>){
     val budget = category.budget
     var usedBudget = 0f
     for (entry in entryList) {
-        usedBudget-= entry.amount //Money spent negative, so we want to add the negative amount (- - = +)to usedBudget
+        usedBudget -= entry.amount //Money spent negative, so we want to add the negative amount (- - = +)to usedBudget
     }
-    H1{Text("${category.name} - Budget")}
-    Div{
-        if (usedBudget < budget) {
+    H1 { Text("${category.name} - Budget") }
+    Div {
+        if (usedBudget <= budget && budget > 0) { //Normal not Spent Budget
             //Money Text
-            Div(attrs={style {
-                display(DisplayStyle.Flex)
-                justifyContent(JustifyContent("space-between"))
-            }}){
-                Div{Text(usedBudget.toString()+"€")}
-                Div{Text(budget.toString()+"€")}
+            MoneyTextDiv {
+                Div { Text(usedBudget.toString() + "€") }
+                Div { Text(budget.toString() + "€") }
             }
-            //Bar
-            Svg(viewBox = "0 0 $width $height"){//For aspect ratio - tries to fill out wherever its in
+            Svg(viewBox = "0 0 $width $height") {
                 Rect(x = 0, y = 0, width = width, height = height, {
                     attr("fill", Color.lightgray.toString())
                 })
-                Rect(x = 0, y = 0, width = usedBudget/budget*width, height = height, {
-                    attr("fill", Color.darkred.toString())
-                })
+                if (0 < usedBudget) // If there is used budget, draw it
+                    Rect(x = 0, y = 0, width = usedBudget / budget * width, height = height, {
+                        attr("fill", "#" + category.color)
+                    })
             }
-        }
-        else{
-            //SpentBudget Text
-            Div(attrs={style {
-                display(DisplayStyle.Flex)
-                justifyContent(JustifyContent("center"))
-            }}){
-                Div{Text("Budget limit for "+category.name+" reached! "+usedBudget.toString()+"€ of "+budget.toString()+"€ Budget spent")}
+        } else if (usedBudget > budget && budget > 0) { //Over Budget
+            MoneyTextDiv {
+                Div { Text("Budget limit for " + category.name + " reached! " + usedBudget.toString() + "€ of " + budget.toString() + "€ Budget spent") }
             }
-            //Bar
-            Svg(viewBox = "0 0 $width $height"){//For aspect ratio - tries to fill out wherever its in
+            Svg(viewBox = "0 0 $width $height") {
                 Rect(x = 0, y = 0, width = width, height = height, {
-                    attr("fill", Color.red.toString())
+                    attr("fill", "#b00020")
+                })
+            }
+        } else if (budget <= 0f) { //No Category View or other unpredictable case
+            MoneyTextDiv {
+                Div { Text(usedBudget.toString() + "€ without category spent") }
+            }
+            Svg(viewBox = "0 0 $width $height") {
+                Rect(x = 0, y = 0, width = width, height = height, {
+                    attr("fill", "#" + category.color)
                 })
             }
         }
+    }
+}
+
+@Composable
+fun MoneyTextDiv(content: @Composable () -> Unit) {
+    Div(attrs = {
+        style {
+            display(DisplayStyle.Flex)
+            justifyContent(JustifyContent("space-between"))
+        }
+    }) {
+        content()
     }
 }
 
