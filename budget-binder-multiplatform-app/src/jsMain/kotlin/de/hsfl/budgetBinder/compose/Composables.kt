25b8769f6b10bb9ec9bed2462627452821810576
Dominik Heckner <dominik.heckner@stud.hs-flensburg.de>
2022-06-13 19:54:11 +0200

    Add ImageList as Composable

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Composables.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Composables.kt
@@ -1,32 +1,33 @@
 package de.hsfl.budgetBinder.compose
 
+import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.common.Category
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import de.hsfl.budgetBinder.compose.theme.AppStylesheet
-import de.hsfl.budgetBinder.presentation.Screen
+import de.hsfl.budgetBinder.presentation.CategoryImageToIcon
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 
 
 /*Main Container for every mayor layout*/
 @Composable
-fun MainFlexContainer(content: @Composable () -> Unit){
-    Div (
+fun MainFlexContainer(content: @Composable () -> Unit) {
+    Div(
         attrs = {
             classes("mdc-top-app-bar--fixed-adjust", AppStylesheet.flexContainer)
         }
-            ){
-        Div (attrs = { classes(AppStylesheet.pufferFlexContainer) })
-        Div (attrs = { classes(AppStylesheet.contentFlexContainer)})
+    ) {
+        Div(attrs = { classes(AppStylesheet.pufferFlexContainer) })
+        Div(attrs = { classes(AppStylesheet.contentFlexContainer) })
         {
             content()
         }
-        Div (attrs = { classes(AppStylesheet.pufferFlexContainer)})
+        Div(attrs = { classes(AppStylesheet.pufferFlexContainer) })
     }
 }
 
 @Composable
-fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () -> Unit){
+fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () -> Unit) {
     Header(
         attrs = {
             classes("mdc-top-app-bar")
@@ -57,6 +58,38 @@ fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () ->
                 }
             ) {
                 navButtons()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryImagesToImageList(onClick : (Category.Image) -> Unit) {
+    Div(
+        attrs = {
+            classes("mdc-card", AppStylesheet.card)
+        }
+    ) {
+        Ul(
+            attrs = {
+                classes("mdc-image-list", "my-image-list")
+            }
+        ) {
+            for (image in Category.Image.values()) {
+                Li(
+                    attrs = {
+                        classes("mdc-image-list__item")
+                    }
+                ) {
+                    Div(
+                        attrs = {
+                            classes("mdc-image-list__image-aspect-container", "mdc-icon-button")
+                            onClick { onClick }
+                        }
+                    ) {
+                        CategoryImageToIcon(image)
+                    }
+                }
             }
         }
     }
