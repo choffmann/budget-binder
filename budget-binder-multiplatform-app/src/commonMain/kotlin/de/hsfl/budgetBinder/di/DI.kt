c4d9bf6c604380587eecbda644d4085411c4a066
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-13 20:08:26 +0200

    Move di into commonMain

---
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/di/DI.kt
@@ -0,0 +1,83 @@
+package de.hsfl.budgetBinder.di
+
+import de.hsfl.budgetBinder.data.client.Client
+import de.hsfl.budgetBinder.data.repository.AuthRepositoryImpl
+import de.hsfl.budgetBinder.data.repository.CategoryRepositoryImpl
+import de.hsfl.budgetBinder.data.repository.EntryRepositoryImpl
+import de.hsfl.budgetBinder.data.repository.UserRepositoryImpl
+import de.hsfl.budgetBinder.domain.repository.AuthRepository
+import de.hsfl.budgetBinder.domain.repository.CategoryRepository
+import de.hsfl.budgetBinder.domain.repository.EntryRepository
+import de.hsfl.budgetBinder.domain.repository.UserRepository
+import de.hsfl.budgetBinder.domain.usecase.*
+import de.hsfl.budgetBinder.presentation.flow.DataFlow
+import de.hsfl.budgetBinder.presentation.flow.RouterFlow
+import de.hsfl.budgetBinder.presentation.login.LoginViewModel
+import de.hsfl.budgetBinder.presentation.viewmodel.*
+import io.ktor.client.engine.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.kodein.di.DI
+import org.kodein.di.bindSingleton
+import org.kodein.di.instance
+
+fun di(ktorEngine: HttpClientEngine) = DI {
+    // scope
+    bindSingleton { CoroutineScope(Dispatchers.Unconfined + SupervisorJob()) }
+
+    // Client
+    bindSingleton { Client(engine = ktorEngine) }
+
+    // Repositories
+    bindSingleton<AuthRepository> { AuthRepositoryImpl(instance()) }
+    bindSingleton<UserRepository> { UserRepositoryImpl(instance()) }
+    bindSingleton<CategoryRepository> { CategoryRepositoryImpl(instance()) }
+    bindSingleton<EntryRepository> { EntryRepositoryImpl(instance()) }
+
+    // AuthUseCase
+    bindSingleton { RegisterUseCase(instance()) }
+    bindSingleton { LoginUseCase(instance()) }
+    bindSingleton { LogoutUseCase(instance()) }
+
+    // UserUseCase
+    bindSingleton { GetMyUserUseCase(instance()) }
+    bindSingleton { ChangeMyUserUseCase(instance()) }
+    bindSingleton { DeleteMyUserUseCase(instance()) }
+
+    // CategoriesUseCase
+    bindSingleton { GetAllCategoriesUseCase(instance()) }
+    bindSingleton { CreateCategoryUseCase(instance()) }
+    bindSingleton { GetCategoryByIdUseCase(instance()) }
+    bindSingleton { ChangeCategoryByIdUseCase(instance()) }
+    bindSingleton { DeleteCategoryByIdUseCase(instance()) }
+    bindSingleton { GetAllEntriesByCategoryUseCase(instance()) }
+
+    // Entries
+    bindSingleton { GetAllEntriesUseCase(instance()) }
+    bindSingleton { CreateNewEntryUseCase(instance()) }
+    bindSingleton { GetEntryByIdUseCase(instance()) }
+    bindSingleton { ChangeEntryByIdUseCase(instance()) }
+    bindSingleton { DeleteEntryByIdUseCase(instance()) }
+
+    bindSingleton { EntriesUseCases(instance(), instance(), instance(), instance(), instance()) }
+    bindSingleton { CategoriesUseCases(instance(), instance(), instance(), instance(), instance(), instance()) }
+    bindSingleton { SettingsUseCases(instance(), instance()) }
+    bindSingleton { LoginUseCases(instance(), instance()) }
+    bindSingleton { DashboardUseCases(instance(), instance()) }
+    bindSingleton { RegisterUseCases(instance(), instance(), instance()) }
+    bindSingleton { NavigateToScreenUseCase() }
+
+    // Flows
+    bindSingleton { RouterFlow(instance()) }
+    bindSingleton { DataFlow(instance()) }
+
+    // ViewModels
+    bindSingleton { LoginViewModel(instance(), instance(), instance(), instance()) }
+    bindSingleton { RegisterViewModel(instance(), instance()) }
+    bindSingleton { SettingsViewModel(instance(), instance()) }
+    bindSingleton { CategoryViewModel(instance(), instance()) }
+    bindSingleton { EntryViewModel(instance(), instance()) }
+    bindSingleton { DashboardViewModel(instance(), instance()) }
+    bindSingleton { NavDrawerViewModel(instance(), instance()) }
+}
--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/compose/Root.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/compose/Root.kt
@@ -6,89 +6,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
-import de.hsfl.budgetBinder.data.client.Client
-import de.hsfl.budgetBinder.data.repository.AuthRepositoryImpl
-import de.hsfl.budgetBinder.data.repository.CategoryRepositoryImpl
-import de.hsfl.budgetBinder.data.repository.EntryRepositoryImpl
-import de.hsfl.budgetBinder.data.repository.UserRepositoryImpl
-import de.hsfl.budgetBinder.domain.repository.AuthRepository
-import de.hsfl.budgetBinder.domain.repository.CategoryRepository
-import de.hsfl.budgetBinder.domain.repository.EntryRepository
-import de.hsfl.budgetBinder.domain.repository.UserRepository
-import de.hsfl.budgetBinder.domain.usecase.*
-import de.hsfl.budgetBinder.presentation.RouterFlow
-import de.hsfl.budgetBinder.presentation.login.LoginViewModel
-import de.hsfl.budgetBinder.presentation.viewmodel.*
+import de.hsfl.budgetBinder.di.di
 import io.ktor.client.engine.cio.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import org.kodein.di.DI
-import org.kodein.di.bindSingleton
 import org.kodein.di.compose.withDI
-import org.kodein.di.instance
-
-val di = DI {
-    // scope
-    bindSingleton { CoroutineScope(Dispatchers.Unconfined + SupervisorJob()) }
-
-    // Client
-    bindSingleton { Client(engine = CIO.create()) }
-
-    // Repositories
-    bindSingleton<AuthRepository> { AuthRepositoryImpl(instance()) }
-    bindSingleton<UserRepository> { UserRepositoryImpl(instance()) }
-    bindSingleton<CategoryRepository> { CategoryRepositoryImpl(instance()) }
-    bindSingleton<EntryRepository> { EntryRepositoryImpl(instance()) }
-
-    // AuthUseCase
-    bindSingleton { RegisterUseCase(instance()) }
-    bindSingleton { LoginUseCase(instance()) }
-    bindSingleton { LogoutUseCase(instance()) }
-
-    // UserUseCase
-    bindSingleton { GetMyUserUseCase(instance()) }
-    bindSingleton { ChangeMyUserUseCase(instance()) }
-    bindSingleton { DeleteMyUserUseCase(instance()) }
-
-    // CategoriesUseCase
-    bindSingleton { GetAllCategoriesUseCase(instance()) }
-    bindSingleton { CreateCategoryUseCase(instance()) }
-    bindSingleton { GetCategoryByIdUseCase(instance()) }
-    bindSingleton { ChangeCategoryByIdUseCase(instance()) }
-    bindSingleton { DeleteCategoryByIdUseCase(instance()) }
-    bindSingleton { GetAllEntriesByCategoryUseCase(instance()) }
-
-    // Entries
-    bindSingleton { GetAllEntriesUseCase(instance()) }
-    bindSingleton { CreateNewEntryUseCase(instance()) }
-    bindSingleton { GetEntryByIdUseCase(instance()) }
-    bindSingleton { ChangeEntryByIdUseCase(instance()) }
-    bindSingleton { DeleteEntryByIdUseCase(instance()) }
-
-    bindSingleton { EntriesUseCases(instance(), instance(), instance(), instance(), instance()) }
-    bindSingleton { CategoriesUseCases(instance(), instance(), instance(), instance(), instance(), instance()) }
-    bindSingleton { SettingsUseCases(instance(), instance()) }
-    bindSingleton { LoginUseCases(instance(), instance()) }
-    bindSingleton { DashboardUseCases(instance(), instance()) }
-    bindSingleton { RegisterUseCases(instance(), instance(), instance()) }
-    bindSingleton { NavigateToScreenUseCase() }
-
-    // Flows
-    bindSingleton { RouterFlow(instance()) }
-
-    // ViewModels
-    bindSingleton { LoginViewModel(instance(), instance(), instance()) }
-    bindSingleton { RegisterViewModel(instance(), instance()) }
-    bindSingleton { SettingsViewModel(instance(), instance()) }
-    bindSingleton { CategoryViewModel(instance(), instance()) }
-    bindSingleton { EntryViewModel(instance(), instance()) }
-    bindSingleton { DashboardViewModel(instance(), instance()) }
-    bindSingleton { NavDrawerViewModel(instance(), instance()) }
-}
 
 @Composable
-fun App() = withDI(di) {
+fun App() = withDI(di(ktorEngine = CIO.create())) {
     val darkTheme = remember { mutableStateOf(false) }
     MaterialTheme(
         colors = if (darkTheme.value) darkColors() else lightColors()
