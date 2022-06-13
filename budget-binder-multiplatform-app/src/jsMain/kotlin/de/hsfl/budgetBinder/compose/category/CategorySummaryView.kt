db5a0632a9be62fcb1830cb5a61322f788d92251
Dominik Heckner <dominik.heckner@stud.hs-flensburg.de>
2022-06-13 16:53:40 +0200

    Add TopBar and MainContainer to CategorySummaryView

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategorySummaryView.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/category/CategorySummaryView.kt
@@ -1,11 +1,15 @@
 package de.hsfl.budgetBinder.compose.category
 
 import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.compose.MainFlexContainer
+import de.hsfl.budgetBinder.compose.theme.AppStylesheet
+import de.hsfl.budgetBinder.compose.topBarMain
 import de.hsfl.budgetBinder.presentation.UiState
-import org.jetbrains.compose.web.dom.Button
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.css.margin
+import org.jetbrains.compose.web.css.marginBottom
+import org.jetbrains.compose.web.css.marginLeft
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.dom.*
 
 
 @Composable
@@ -13,36 +17,106 @@ fun CategorySummaryView(
     state: State<Any>,
     onBackButton: () -> Unit,
     onEditButton: () -> Unit,
-    onCategoryCreateButton: () -> Unit
+    onCategoryCreateButton: () -> Unit,
+    onChangeToDashboard: () -> Unit,
+    onChangeToCategory: () -> Unit,
+    onChangeToSettings: () -> Unit
 ) {
     val viewState by remember { state }
-    H1{Text("CategorySummaryView")}
-    Div {
-        when (viewState) {
-            is UiState.Success<*> -> {
-                Text((viewState as UiState.Success<*>).element.toString())
+
+    topBarMain(
+        logoButton = {
+            Img(
+                src = "images/Logo.png", alt = "Logo", attrs = {
+                    classes("mdc-icon-button", AppStylesheet.image)
+                    onClick { onChangeToDashboard() }
+                }
+            )
+        }, navButtons = {
+            Button(
+                attrs = {
+                    classes("mdc-button", "mdc-button--raised", "mdc-top-app-bar__navigation-icon")
+                    onClick { onChangeToCategory() }
+                }
+            ) {
+                Span(
+                    attrs = {
+                        classes("mdc-button__label")
+                    }
+                ) {
+                    Text("Categories")
+                }
             }
-            is UiState.Error -> {
-                Text((viewState as UiState.Error).error)
+            Button(
+                attrs = {
+                    classes("mdc-button", "mdc-button--raised", "mdc-top-app-bar__navigation-icon")
+                    onClick { onChangeToSettings() }
+                }
+            ) {
+                Span(
+                    attrs = {
+                        classes("mdc-button__label")
+                    }
+                ) {
+                    Text("Settings")
+                }
             }
-            is UiState.Loading -> {
-                //CircularProgressIndicator()
+        })
+
+    MainFlexContainer {
+        Div(attrs = {
+            classes("mdc-card", AppStylesheet.card)
+        }
+        ) {
+            H1(
+                attrs = {
+                    style { margin(2.percent) }
+                }
+            ) { Text(" Category Summary") }
+            Button(attrs = {
+                classes("mdc-button", "mdc-button--raised")
+                onClick { onCategoryCreateButton() }
+                style { margin(2.percent) }
+            }) {
+                Text("Create Category")
             }
-        }
-        Button(attrs = {
-            onClick { onBackButton() }
-        }) {
-            Text("Back to Dashboard")
-        }
-        Button(attrs = {
-            onClick { onEditButton() }
-        }) {
-            Text("Edit Category (Needs to be set for each category)")
-        }
-        Button(attrs = {
-            onClick { onCategoryCreateButton() }
-        }) {
-            Text("Create Category")
+            Div {
+                when (viewState) {
+                    is UiState.Success<*> -> {
+                        Text((viewState as UiState.Success<*>).element.toString())
+                    }
+                    is UiState.Error -> {
+                        Text((viewState as UiState.Error).error)
+                    }
+                    is UiState.Loading -> {
+                        //CircularProgressIndicator()
+                    }
+                }
+                Div(attrs = {
+                    classes("mdc-card", AppStylesheet.card)
+                }
+                ) {
+                    Text("Kat 1")
+                    Button(attrs = {
+                        classes("mdc-button", "mdc-button--raised")
+                        onClick { onEditButton() }
+                    }) {
+                        Text("Edit Category (Needs to be set for each category)")
+                    }
+                }
+                Div(attrs = {
+                    classes("mdc-card", AppStylesheet.card)
+                }
+                ) {
+                    Text("Kat 2")
+                    Button(attrs = {
+                        classes("mdc-button", "mdc-button--raised")
+                        onClick { onEditButton() }
+                    }) {
+                        Text("Edit Category (Needs to be set for each category)")
+                    }
+                }
+            }
         }
     }
 }
