ba74c10e5f3e3ded41cad5f03f468bc310e8c1b8
Fabian Petersen <fabian@nf-petersen.de>
2022-06-14 14:41:12 +0200

    make the Android Application accessible through a singleton

--- budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/BudgetBinderApplication.kt
+++ budget-binder-multiplatform-app/src/androidMain/kotlin/de/hsfl/budgetBinder/android/BudgetBinderApplication.kt
@@ -3,5 +3,12 @@ package de.hsfl.budgetBinder.android
 import android.app.Application
 
 class BudgetBinderApplication: Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+    companion object {
+        lateinit var instance: BudgetBinderApplication
+            private set
+    }
 }
