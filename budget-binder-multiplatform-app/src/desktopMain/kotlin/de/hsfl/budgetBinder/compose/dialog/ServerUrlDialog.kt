27003e875d6a33f4e7ccf1cbc23dfacbb2c1eee3
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-14 15:17:37 +0200

    Implement SDesktop ServerUrlDialog.kt

--- budget-binder-multiplatform-app/src/desktopMain/kotlin/de/hsfl/budgetBinder/compose/dialog/ServerUrlDialog.kt
+++ budget-binder-multiplatform-app/src/desktopMain/kotlin/de/hsfl/budgetBinder/compose/dialog/ServerUrlDialog.kt
@@ -1,8 +1,15 @@
 package de.hsfl.budgetBinder.compose.dialog
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Dns
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 actual fun ServerUrlDialog(
     value: String,
@@ -11,5 +18,29 @@ actual fun ServerUrlDialog(
     onConfirm: () -> Unit,
     onDissmiss: () -> Unit
 ) {
-
+    if (openDialog) {
+        AlertDialog(
+            title = { Text(text = "Please Enter Server URL", style = MaterialTheme.typography.body2) },
+            onDismissRequest = onDissmiss,
+            buttons = {
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    TextButton(
+                        modifier = Modifier.padding(bottom = 8.dp, end = 8.dp).align(Alignment.CenterEnd),
+                        onClick = onConfirm
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            },
+            text = {
+                TextField(
+                    value = value,
+                    onValueChange = onValueChange,
+                    label = { Text("Server URL") },
+                    leadingIcon = { Icon(Icons.Filled.Dns, contentDescription = null) },
+                    singleLine = true
+                )
+            }
+        )
+    }
 }
