510180e755f62719d4228c950d49ba902f12e629
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-15 22:58:31 +0200

    Implement DeleteUserDialog

--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/settings/SettingsMenuView.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/settings/SettingsMenuView.kt
@@ -2,6 +2,8 @@ package de.hsfl.budgetBinder.screens.settings
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import de.hsfl.budgetBinder.compose.dialog.DeleteUserDialog
 import de.hsfl.budgetBinder.di
 import de.hsfl.budgetBinder.presentation.flow.DataFlow
 import de.hsfl.budgetBinder.presentation.viewmodel.settings.SettingsEvent
@@ -24,6 +28,7 @@ fun SettingsMenuView(modifier: Modifier = Modifier) {
     val viewModel: SettingsViewModel by di.instance()
     val dataFlow: DataFlow by di.instance()
     val darkMode = dataFlow.darkModeState.collectAsState(scope.coroutineContext)
+    val dialogState = viewModel.dialogState.collectAsState()
 
     Column(modifier = modifier) {
         Divider()
@@ -49,5 +54,21 @@ fun SettingsMenuView(modifier: Modifier = Modifier) {
         ListItem(modifier = Modifier.clickable(onClick = { viewModel.onEvent(SettingsEvent.OnLogoutAllDevices) }),
             text = { Text("Logout on all device") },
             icon = { Icon(Icons.Filled.ExitToApp, contentDescription = null) })
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            modifier = Modifier.padding(start = 8.dp),
+            text = "Danger Zone",
+            color = Color.Red,
+            style = MaterialTheme.typography.subtitle2
+        )
+        Divider()
+        ListItem(modifier = Modifier.clickable(onClick = { viewModel.onEvent(SettingsEvent.OnDeleteUser) }),
+            text = { Text("Delete my User") },
+            icon = { Icon(Icons.Filled.Delete, contentDescription = null) })
     }
+    DeleteUserDialog(
+        openDialog = dialogState.value,
+        onDismiss = { viewModel.onEvent(SettingsEvent.OnDeleteDialogDismiss) },
+        onConfirm = { viewModel.onEvent(SettingsEvent.OnDeleteDialogConfirm) }
+    )
 }
