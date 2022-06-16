d76f541014615ca9b505b51bd2fee1c6743922e2
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-17 00:15:00 +0200

    Implement DashboardView

--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/dashboard/DashboardComponent.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/dashboard/DashboardComponent.kt
@@ -1,34 +1,77 @@
 package de.hsfl.budgetBinder.screens.dashboard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import de.hsfl.budgetBinder.common.Entry
 import de.hsfl.budgetBinder.di
+import de.hsfl.budgetBinder.presentation.UiEvent
 import de.hsfl.budgetBinder.presentation.flow.DataFlow
-import de.hsfl.budgetBinder.presentation.viewmodel.DashboardViewModel
+import de.hsfl.budgetBinder.presentation.viewmodel.dashboard.DashboardEvent
+import de.hsfl.budgetBinder.presentation.viewmodel.dashboard.DashboardViewModel
+import kotlinx.coroutines.flow.collectLatest
 import org.kodein.di.instance
 
 @Composable
 fun DashboardComponent() {
-    val scope = rememberCoroutineScope()
-    val dataFlow: DataFlow by di.instance()
-    val userState = dataFlow.userState.collectAsState(scope.coroutineContext)
     val viewModel: DashboardViewModel by di.instance()
-    val logoutState = viewModel.state.collectAsState(scope.coroutineContext)
+    val categoryList = viewModel.categoryListSate.collectAsState()
+    val entryList = viewModel.entryListState.collectAsState()
+    val focusedCategory = viewModel.entryListState.collectAsState()
+    val scaffoldState = rememberScaffoldState()
+    val loadingState = remember { mutableStateOf(false) }
 
-    Column {
-        Text(userState.value.toString())
-        Row {
-            Button(onClick = { viewModel.logOut(true) }) {
-                Text("Logout")
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                UiEvent.ShowLoading -> loadingState.value = true
+                else -> loadingState.value = false
             }
-            Button(onClick = { viewModel.getMyUser() }) {
-                Text("Update")
+        }
+    }
+
+    if(loadingState.value) {
+        LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { viewModel.onEvent(DashboardEvent.OnEntryCreate) }) {
+                Icon(Icons.Default.Add, contentDescription = null)
             }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        isFloatingActionButtonDocked = true
+    ) {
+        Column {
+            EntryList(entryList = entryList.value.entryList)
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun EntryList(entryList: List<Entry>) {
+    when {
+        entryList.isEmpty() -> Text("No entries here, you can create an new entry.")
+    }
+    LazyColumn {
+        items(entryList) { entry ->
+            ListItem(
+                text = { Text(entry.name) },
+                icon = { Icon(Icons.Default.ShoppingCart, contentDescription = null) },
+                trailing = { Text("${entry.amount} €") }
+            )
         }
     }
 }
