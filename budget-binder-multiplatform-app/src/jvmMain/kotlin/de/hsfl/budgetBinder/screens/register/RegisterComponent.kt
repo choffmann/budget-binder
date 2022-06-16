faf4995097b5343c7e8842f96924e2aeca7dbe29
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-16 16:09:29 +0200

    Implement second textfield in RegisterView

--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/register/RegisterComponent.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/screens/register/RegisterComponent.kt
@@ -3,6 +3,8 @@ package de.hsfl.budgetBinder.screens.register
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,13 +22,12 @@ import org.kodein.di.instance
 
 @Composable
 fun RegisterComponent() {
-    val scope = rememberCoroutineScope()
     val viewModel: RegisterViewModel by di.instance()
-    val firstNameTextState = viewModel.firstNameText.collectAsState(scope.coroutineContext)
-    val lastNameTextState = viewModel.lastNameText.collectAsState(scope.coroutineContext)
-    val emailTextState = viewModel.emailText.collectAsState(scope.coroutineContext)
-    val passwordTextState = viewModel.passwordText.collectAsState(scope.coroutineContext)
-    val scaffoldState = rememberScaffoldState()
+    val firstNameTextState = viewModel.firstNameText.collectAsState()
+    val lastNameTextState = viewModel.lastNameText.collectAsState()
+    val emailTextState = viewModel.emailText.collectAsState()
+    val passwordTextState = viewModel.passwordText.collectAsState()
+    val confirmedPasswordTextState = viewModel.confirmedPasswordText.collectAsState()
     val loadingState = remember { mutableStateOf(false) }
 
     LaunchedEffect(key1 = true) {
@@ -36,58 +37,73 @@ fun RegisterComponent() {
                     // TODO: Refactor this, it's working but ahh
                     loadingState.value = true
                 }
-                is UiEvent.ShowError -> {
-                    loadingState.value = false
-                    scaffoldState.snackbarHostState.showSnackbar(message = event.msg, actionLabel = "Dissmiss")
-                }
+                else -> loadingState.value = false
             }
         }
     }
 
-    Scaffold(scaffoldState = scaffoldState) {
-        if (loadingState.value) {
-            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
-        }
-        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            AppIcon(modifier = Modifier.size(128.dp).padding(8.dp))
-            Text(text = "Hello there 👋", style = MaterialTheme.typography.h5)
-            Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(
-                value = firstNameTextState.value.firstName,
-                onValueChange = { viewModel.onEvent(RegisterEvent.EnteredFirstname(it)) },
-                label = { Text("Firstname") },
-                singleLine = true
-            )
-            OutlinedTextField(
-                value = lastNameTextState.value.lastName,
-                onValueChange = { viewModel.onEvent(RegisterEvent.EnteredLastname(it)) },
-                label = { Text("Lastname") },
-                singleLine = true
-            )
-            EmailTextField(
-                value = emailTextState.value.email,
-                onValueChange = { viewModel.onEvent(RegisterEvent.EnteredEmail(it)) },
-                label = { Text("Email") },
-                isError = !emailTextState.value.emailValid,
-                enabled = !loadingState.value
-            )
-            OutlinedTextField(
-                value = passwordTextState.value.password,
-                onValueChange = { viewModel.onEvent(RegisterEvent.EnteredPassword(it)) },
-                label = { Text("Password") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                singleLine = true
-            )
-            Button(onClick = { viewModel.onEvent(RegisterEvent.OnRegister) }) {
-                Text("Register")
-            }
-            Box(modifier = Modifier.fillMaxSize()) {
-                TextButton(modifier = Modifier.align(Alignment.BottomCenter), onClick = {
-                    viewModel.onEvent(RegisterEvent.OnLoginScreen)
-                }) {
-                    Text("Or Login with your Account")
+    if (loadingState.value) {
+        LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+    }
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        AppIcon(modifier = Modifier.size(128.dp).padding(8.dp))
+        Text(text = "Hello there 👋", style = MaterialTheme.typography.h5)
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = firstNameTextState.value.firstName,
+            onValueChange = { viewModel.onEvent(RegisterEvent.EnteredFirstname(it)) },
+            label = { Text("Firstname") },
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = lastNameTextState.value.lastName,
+            onValueChange = { viewModel.onEvent(RegisterEvent.EnteredLastname(it)) },
+            label = { Text("Lastname") },
+            singleLine = true
+        )
+        EmailTextField(
+            value = emailTextState.value.email,
+            onValueChange = { viewModel.onEvent(RegisterEvent.EnteredEmail(it)) },
+            label = { Text("Email") },
+            isError = !emailTextState.value.emailValid,
+            enabled = !loadingState.value
+        )
+        OutlinedTextField(
+            value = passwordTextState.value.password,
+            onValueChange = { viewModel.onEvent(RegisterEvent.EnteredPassword(it)) },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = confirmedPasswordTextState.value.confirmedPassword,
+            onValueChange = { viewModel.onEvent(RegisterEvent.EnteredConfirmedPassword(it))},
+            label = { Text("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true,
+            isError = !confirmedPasswordTextState.value.confirmedPasswordValid,
+            enabled = !loadingState.value,
+            trailingIcon = {
+                if (!confirmedPasswordTextState.value.confirmedPasswordValid) {
+                    Icon(Icons.Default.Info, contentDescription = null)
                 }
+            }
+        )
+        if(!confirmedPasswordTextState.value.confirmedPasswordValid) {
+            Text(text = "Password didn't match", style = MaterialTheme.typography.caption, color = MaterialTheme.colors.error)
+        }
+
+
+        Button(onClick = { viewModel.onEvent(RegisterEvent.OnRegister) }) {
+            Text("Register")
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            TextButton(modifier = Modifier.align(Alignment.BottomCenter), onClick = {
+                viewModel.onEvent(RegisterEvent.OnLoginScreen)
+            }) {
+                Text("Or Login with your Account")
             }
         }
     }
