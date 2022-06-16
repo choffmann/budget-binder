16cfa8df18bc763597a5b481aafb2eca78659b61
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-16 11:57:18 +0200

    Reimplement Loading

--- budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/Root.kt
+++ budget-binder-multiplatform-app/src/jvmMain/kotlin/de/hsfl/budgetBinder/Root.kt
@@ -1,10 +1,12 @@
 package de.hsfl.budgetBinder
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import de.hsfl.budgetBinder.di.kodein
 import de.hsfl.budgetBinder.presentation.UiEvent
 import de.hsfl.budgetBinder.presentation.flow.DataFlow
@@ -23,11 +25,16 @@ fun App() = withDI(di) {
     val uiEventFlow: UiEventSharedFlow by di.instance()
     val darkTheme = dataFlow.darkModeState.collectAsState(scope.coroutineContext)
     val scaffoldState = rememberScaffoldState()
+    val loadingState = remember { mutableStateOf(false) }
 
     LaunchedEffect(key1 = true) {
         uiEventFlow.eventFlow.collectLatest { event ->
             when (event) {
-                is UiEvent.ShowError -> scaffoldState.snackbarHostState.showSnackbar(message = event.msg)
+                is UiEvent.ShowLoading -> loadingState.value = true
+                is UiEvent.ShowError -> {
+                    loadingState.value = false
+                    scaffoldState.snackbarHostState.showSnackbar(message = event.msg)
+                }
             }
         }
     }
@@ -35,13 +42,16 @@ fun App() = withDI(di) {
     MaterialTheme(
         colors = if (darkTheme.value) darkColors() else lightColors()
     ) {
+        if (loadingState.value) {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+        }
         Scaffold(
             scaffoldState = scaffoldState,
             topBar = {
                 // TODO: Show NavDrawer not on Login und Register
                 TopAppBar(title = { Text("Budget Binder") }, navigationIcon = {
                     IconButton(onClick = {
-                        // TODO: Implement NavBar Logic
+                        // TODO: Implement NavBar
                     }) {
                         Icon(Icons.Filled.Menu, contentDescription = null)
                     }
