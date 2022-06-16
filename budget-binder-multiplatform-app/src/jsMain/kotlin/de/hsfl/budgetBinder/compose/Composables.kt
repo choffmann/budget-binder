140a4182da5e866fcf205257195e4dfef54618d2
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-16 11:54:28 +0200

    Add first non-working version of snackbar-feedback

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Composables.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Composables.kt
@@ -11,23 +11,23 @@ import org.jetbrains.compose.web.dom.*
 
 /*Main Container for every mayor layout*/
 @Composable
-fun MainFlexContainer(content: @Composable () -> Unit){
-    Div (
+fun MainFlexContainer(content: @Composable () -> Unit) {
+    Div(
         attrs = {
             classes("mdc-top-app-bar--fixed-adjust", AppStylesheet.flexContainer)
         }
-            ){
-        Div (attrs = { classes(AppStylesheet.pufferFlexContainer) })
-        Div (attrs = { classes(AppStylesheet.contentFlexContainer)})
+    ) {
+        Div(attrs = { classes(AppStylesheet.pufferFlexContainer) })
+        Div(attrs = { classes(AppStylesheet.contentFlexContainer) })
         {
             content()
         }
-        Div (attrs = { classes(AppStylesheet.pufferFlexContainer)})
+        Div(attrs = { classes(AppStylesheet.pufferFlexContainer) })
     }
 }
 
 @Composable
-fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () -> Unit){
+fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () -> Unit) {
     Header(
         attrs = {
             classes("mdc-top-app-bar")
@@ -63,14 +63,51 @@ fun topBarMain(logoButton: @Composable () -> Unit, navButtons: @Composable () ->
     }
 }
 
+
+///* Gives a material icon based on the icon name*///
 @Composable
-fun Icon (icon_name: String){
+fun Icon(icon_name: String) {
     Span(
         attrs = {
             classes("material-icons")
             style {
                 width(24.px)
-                height(24.px) }
+                height(24.px)
+            }
         }
-    ) {Text(icon_name)}
+    ) { Text(icon_name) }
 }
+
+
+// Snackbar that shows msg
+@Composable
+fun FeedbackSnackbar(msg: String) {
+    Aside(attrs = { classes("mdc-snackbar") }) {
+        Div(attrs = {
+            classes("mdc-snackbar__surface")
+            attr(attr = "role", value = "status")
+            attr(attr = "aria-relevant", value = "additions")
+        }) {
+            Div(attrs = {
+                classes("mdc-snackbar__label")
+                attr(attr = "aria-atomic", value = "false")
+            }) {
+                Text(msg)
+            }
+            Div(attrs = {
+                classes("mdc-snackbar__actions")
+                attr(attr = "aria-atomic", value = "true")
+            }) {
+                Button(attrs = { classes("mdc-button", "mdc-snackbar__action") }) {
+                    Div(attrs = {
+                        classes("mdc-button__ripple")
+                    })
+                    Span(attrs = { classes("mdc-button__label") })
+                    { Text("Dismiss") }
+                }
+            }
+        }
+    }
+}
+
+
