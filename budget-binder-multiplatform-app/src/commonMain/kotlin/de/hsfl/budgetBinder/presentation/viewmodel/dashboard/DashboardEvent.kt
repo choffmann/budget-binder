da6bf3a7fb508e0688e5b4508d8dfc4915cf4960
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-18 00:56:27 +0200

    Implement older entries on request

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/dashboard/DashboardEvent.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/dashboard/DashboardEvent.kt
@@ -5,6 +5,8 @@ sealed class DashboardEvent {
     object OnPrevCategory : DashboardEvent()
     object OnNextCategory : DashboardEvent()
     object OnRefresh: DashboardEvent()
+    object OnLoadMore: DashboardEvent()
     data class OnEntry(val id: Int) : DashboardEvent()
+    data class OnEntryDelete(val id: Int): DashboardEvent()
     object OnEntryCreate : DashboardEvent()
 }
