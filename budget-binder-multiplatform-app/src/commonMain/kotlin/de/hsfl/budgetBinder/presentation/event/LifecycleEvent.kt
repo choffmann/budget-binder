bb362698d16390aa890ef068958ab8b6010b5322
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-20 12:25:52 +0200

    Add Comments

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/event/LifecycleEvent.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/event/LifecycleEvent.kt
@@ -1,10 +1,19 @@
 package de.hsfl.budgetBinder.presentation.event
 
 sealed class LifecycleEvent {
+    // Call on LaunchEffect
     object OnLaunch: LifecycleEvent()
+
+    // Call on DisposableEffect
     object OnDispose: LifecycleEvent()
 }
 
+
+/**
+ * Callback on launch or on dispose
+ * @param onLaunch Call on Launch
+ * @param onDispose Call on Dispose
+ */
 fun LifecycleEvent.handleLifeCycle(onLaunch: () -> Unit, onDispose: () -> Unit) {
     when (this) {
         is LifecycleEvent.OnLaunch -> onLaunch()
