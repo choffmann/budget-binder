6353e029f435556a125e20b2743e529143cc84b6
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-20 22:41:37 +0200

    Fix bug where dashboard automatically switches to EntryCreateScreen

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Router.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Router.kt
@@ -21,14 +21,17 @@ fun Router() {
     val scope = rememberCoroutineScope()
     val routerFlow: RouterFlow by di.instance()
     val screenState = routerFlow.state.collectAsState(scope.coroutineContext)
+    console.log(screenState.value)
     when (screenState.value) {
         is Screen.Welcome -> {}
         is Screen.Register -> RegisterComponent()
         is Screen.Login -> LoginComponent()
         is Screen.Dashboard -> DashboardComponent()
-        //is Screen.Settings, is Screen.SettingsChangeUserData -> SettingsComponent(screenState = screenState)
-        //is Screen.CategorySummary, is Screen.CategoryEdit, is Screen.CategoryCreate, is Screen.CategoryCreateOnRegister -> CategoryComponent(screenState = screenState)
-        //is Screen.EntryCreate, is Screen.EntryEdit, is Screen.EntryOverview -> EntryComponent(screenState = screenState)
-        else -> {}
+        is Screen.Settings, is Screen.SettingsChangeUserData -> Text("Settings")//SettingsComponent(screenState = screenState)
+        is Screen.CategorySummary, is Screen.CategoryEdit, is Screen.CategoryCreate, is Screen.CategoryCreateOnRegister ->Text("Old Category") //CategoryComponent(screenState = screenState)
+        is Screen.EntryCreate, is Screen.EntryEdit, is Screen.EntryOverview -> Text("Old Entry")//EntryComponent(screenState = screenState)
+        is Screen.Category -> Text("New Category")
+        is Screen.Entry -> Text("New Entry")
+        else -> {Text("No known Screen! Check if the screen you're trying to reach is in the ScreenRouter")}
     }
 }
