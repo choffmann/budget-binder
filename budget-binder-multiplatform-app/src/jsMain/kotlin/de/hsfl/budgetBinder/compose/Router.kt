a11c0d0a59d65851da8ee495a17186b973accdb4
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-20 21:49:11 +0200

    Update LoginComponent to work with new LoginViewModel, change Router accordingly

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Router.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/Router.kt
@@ -2,6 +2,8 @@ package de.hsfl.budgetBinder.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import de.hsfl.budgetBinder.compose.category.CategoryComponent
 import de.hsfl.budgetBinder.compose.dashboard.DashboardComponent
 import de.hsfl.budgetBinder.compose.entry.EntryComponent
@@ -9,17 +11,24 @@ import de.hsfl.budgetBinder.compose.login.LoginComponent
 import de.hsfl.budgetBinder.compose.register.RegisterComponent
 import de.hsfl.budgetBinder.compose.settings.SettingsComponent
 import de.hsfl.budgetBinder.presentation.Screen
+import de.hsfl.budgetBinder.presentation.flow.RouterFlow
+import di
+import org.jetbrains.compose.web.dom.Text
+import org.kodein.di.instance
 
 @Composable
-fun Router(screenState: MutableState<Screen>) {
+fun Router() {
+    val scope = rememberCoroutineScope()
+    val routerFlow: RouterFlow by di.instance()
+    val screenState = routerFlow.state.collectAsState(scope.coroutineContext)
     when (screenState.value) {
         is Screen.Welcome -> {}
-        is Screen.Register -> RegisterComponent(screenState = screenState)
-        is Screen.Login -> LoginComponent(screenState = screenState)
-        is Screen.Dashboard -> DashboardComponent(screenState = screenState)
-        is Screen.Settings, is Screen.SettingsChangeUserData -> SettingsComponent(screenState = screenState)
-        is Screen.CategorySummary, is Screen.CategoryEdit, is Screen.CategoryCreate, is Screen.CategoryCreateOnRegister -> CategoryComponent(screenState = screenState)
-        is Screen.EntryCreate, is Screen.EntryEdit, is Screen.EntryOverview -> EntryComponent(screenState = screenState)
+        is Screen.Register -> RegisterComponent()
+        is Screen.Login -> LoginComponent()
+        is Screen.Dashboard -> Text("Dashboard")//DashboardComponent()
+        //is Screen.Settings, is Screen.SettingsChangeUserData -> SettingsComponent(screenState = screenState)
+        //is Screen.CategorySummary, is Screen.CategoryEdit, is Screen.CategoryCreate, is Screen.CategoryCreateOnRegister -> CategoryComponent(screenState = screenState)
+        //is Screen.EntryCreate, is Screen.EntryEdit, is Screen.EntryOverview -> EntryComponent(screenState = screenState)
         else -> {}
     }
 }
--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/login/LoginComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/login/LoginComponent.kt
@@ -1,29 +1,189 @@
 package de.hsfl.budgetBinder.compose.login
 import androidx.compose.runtime.*
+import de.hsfl.budgetBinder.compose.MainFlexContainer
+import de.hsfl.budgetBinder.compose.theme.AppStylesheet
 import de.hsfl.budgetBinder.presentation.Screen
+import de.hsfl.budgetBinder.presentation.UiEvent
+import de.hsfl.budgetBinder.presentation.UiState
+import de.hsfl.budgetBinder.presentation.viewmodel.login.LoginEvent
 import de.hsfl.budgetBinder.presentation.viewmodel.login.LoginViewModel
 import di
+import kotlinx.coroutines.flow.collectLatest
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.*
 import org.kodein.di.compose.withDI
 import org.kodein.di.instance
 
 
 @Composable
-fun LoginComponent(screenState: MutableState<Screen>) = withDI(di) {
-    /*val scope = CoroutineScope(Dispatchers.Unconfined + SupervisorJob())
-    val loginUseCase: LoginUseCase by di.instance()
-    val getMyUserUseCase : GetMyUserUseCase by di.instance()
-    val viewModel = LoginViewModel(scope,loginUseCase, getMyUserUseCase)
-    val viewState = viewModel.state.collectAsState(scope)*/
+fun LoginComponent() {
     val scope = rememberCoroutineScope()
     val viewModel: LoginViewModel by di.instance()
-    val viewState = viewModel.state.collectAsState(scope)
+    val emailTextState = viewModel.emailText.collectAsState(scope.coroutineContext)
+    val passwordTextState = viewModel.passwordText.collectAsState(scope.coroutineContext)
+    val loadingState = remember { mutableStateOf(false) }
 
-    LoginView(
-        state = viewState,
-        onLoginButtonPressed = { email, password ->
-            viewModel.login(email, password)
-        },
-        onLoginSuccess = { screenState.value = Screen.Dashboard },
-        onChangeToRegister = { screenState.value = Screen.Register }
-    )
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UiEvent.ShowLoading -> loadingState.value = true
+                else -> loadingState.value = false
+            }
+        }
+    }
+
+    if (loadingState.value) {
+        Text("Loading")
+        //LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+    }
+    //Body
+    Header(
+        attrs = {
+            classes("mdc-top-app-bar")
+        }
+    ) {
+        Div(
+            attrs = {
+                classes("mdc-top-app-bar__row")
+            }
+        ) {
+            Section(
+                attrs = {
+                    classes("mdc-top-app-bar__section", "mdc-top-app-bar__section--align-start")
+                }
+            ) {
+                Img(
+                    src = "images/Logo.png", alt = "Logo", attrs = {
+                        classes("mdc-icon-button", AppStylesheet.image)
+                    }
+                )
+                Span(
+                    attrs = {
+                        classes("mdc-top-app-bar__title")
+                    }
+                ) {
+                    Text("Budget-Binder")
+                }
+            }
+            Section(
+                attrs = {
+                    classes("mdc-top-app-bar__section", "mdc-top-app-bar__section--align-end")
+                }
+            ) {
+                Button(
+                    attrs = {
+                        classes("mdc-button", "mdc-button--raised", "mdc-top-app-bar__navigation-icon")
+                        onClick { viewModel.onEvent(LoginEvent.OnRegisterScreen) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-button__label")
+                        }
+                    ) {
+                        Text("Register Instead")
+                    }
+                }
+            }
+        }
+    }
+
+    MainFlexContainer {
+        // -- Login Form --
+        H1 { Text(" Login") }
+        Form(
+            attrs = {
+                this.addEventListener("submit") {
+                    console.log("$emailTextState, $passwordTextState")
+                    viewModel.onEvent(LoginEvent.OnLogin)
+                    it.preventDefault()
+                }
+            }
+        ) {
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Email") }
+                    EmailInput(value = emailTextState.value.email,
+                        attrs = {
+                            classes("mdc-text-field__input")
+                            onInput {
+                                viewModel.onEvent(LoginEvent.EnteredEmail(it.value))
+
+                            }
+                        })
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Password") }
+                    PasswordInput(value = passwordTextState.value.password,
+                        attrs = {
+                            classes("mdc-text-field__input")
+                            onInput {
+                                viewModel.onEvent(LoginEvent.EnteredPassword(it.value))
+                            }
+                        })
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                SubmitInput(
+                    attrs = {
+                        classes("mdc-button", "mdc-button--raised")
+                        value("Submit")
+                    })
+            }
+        }
+    }
 }
