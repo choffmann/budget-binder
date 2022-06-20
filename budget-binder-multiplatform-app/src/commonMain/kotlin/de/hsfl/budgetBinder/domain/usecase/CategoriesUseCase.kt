3fa67d56cc5eb750979794cbd9c94096978a9b77
Cedrik Hoffmann <hoffmann@geograt.de>
2022-06-20 20:22:39 +0200

    Refactor CategoriesUseCase.kt

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/domain/usecase/CategoriesUseCase.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/domain/usecase/CategoriesUseCase.kt
@@ -1,181 +1,51 @@
 package de.hsfl.budgetBinder.domain.usecase
 
-import de.hsfl.budgetBinder.common.Category
-import de.hsfl.budgetBinder.common.DataResponse
-import de.hsfl.budgetBinder.common.Entry
-import de.hsfl.budgetBinder.common.ErrorModel
+import de.hsfl.budgetBinder.common.*
 import de.hsfl.budgetBinder.domain.repository.CategoryRepository
-import io.ktor.http.*
-import io.ktor.utils.io.errors.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class GetAllCategoriesUseCase(private val repository: CategoryRepository) {
-    fun categories(): Flow<DataResponse<List<Category>>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.getAllCategories().let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
-        }
-    }
-
-    fun categories(period: String): Flow<DataResponse<List<Category>>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.getAllCategories(period).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
+    operator fun invoke(period: String? = null): Flow<DataResponse<List<Category>>> = flow {
+        useCaseHelper {
+            period?.let {
+                repository.getAllCategories(period = it)
+            } ?: repository.getAllCategories()
         }
     }
 }
 
 class CreateCategoryUseCase(private val repository: CategoryRepository) {
     operator fun invoke(category: Category.In): Flow<DataResponse<Category>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.createNewCategory(category).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
-        }
+        useCaseHelper { repository.createNewCategory(category) }
     }
 }
 
 class GetCategoryByIdUseCase(private val repository: CategoryRepository) {
     operator fun invoke(id: Int): Flow<DataResponse<Category>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.getCategoryById(id).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
-        }
+        useCaseHelper { repository.getCategoryById(id) }
     }
 }
 
 class ChangeCategoryByIdUseCase(private val repository: CategoryRepository) {
     operator fun invoke(category: Category.Patch, id: Int): Flow<DataResponse<Category>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.changeCategoryById(category, id).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
-        }
+        useCaseHelper { repository.changeCategoryById(category, id) }
     }
 }
 
+
 class DeleteCategoryByIdUseCase(private val repository: CategoryRepository) {
     operator fun invoke(id: Int): Flow<DataResponse<Category>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            repository.deleteCategoryById(id).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
-        }
+        useCaseHelper { repository.deleteCategoryById(id) }
     }
 }
 
 class GetAllEntriesByCategoryUseCase(private val repository: CategoryRepository) {
     operator fun invoke(id: Int?, period: String? = null): Flow<DataResponse<List<Entry>>> = flow {
-        try {
-            emit(DataResponse.Loading())
-            period?.let { period ->
-                repository.getEntriesFromCategory(id, period).let { response ->
-                    response.data?.let {
-                        emit(DataResponse.Success(it))
-                    } ?: response.error!!.let { error ->
-                        when (error.code) {
-                            HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                            else -> emit(DataResponse.Error(error))
-                        }
-                    }
-                }
-            } ?: repository.getEntriesFromCategory(id).let { response ->
-                response.data?.let {
-                    emit(DataResponse.Success(it))
-                } ?: response.error!!.let { error ->
-                    when (error.code) {
-                        HttpStatusCode.Unauthorized.value -> emit(DataResponse.Unauthorized(error))
-                        else -> emit(DataResponse.Error(error))
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            emit(DataResponse.Error(ErrorModel("Couldn't reach the server")))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emit(DataResponse.Error(ErrorModel("Something went wrong")))
+        useCaseHelper {
+            period?.let {
+                repository.getEntriesFromCategory(id, period)
+            } ?: repository.getEntriesFromCategory(id)
         }
     }
 }
