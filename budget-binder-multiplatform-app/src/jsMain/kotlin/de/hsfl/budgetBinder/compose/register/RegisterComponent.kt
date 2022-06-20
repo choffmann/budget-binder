5654d2822959d3a5882f1e40d25596615754c02d
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-20 21:22:49 +0200

    Update RegisterComponent.kt to work with new RegisterViewModel

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/register/RegisterComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/register/RegisterComponent.kt
@@ -2,31 +2,267 @@ package de.hsfl.budgetBinder.compose.register
 
 import androidx.compose.runtime.*
 import de.hsfl.budgetBinder.common.User
+import de.hsfl.budgetBinder.compose.MainFlexContainer
+import de.hsfl.budgetBinder.compose.theme.AppStylesheet
 
 import de.hsfl.budgetBinder.presentation.Screen
+import de.hsfl.budgetBinder.presentation.UiEvent
+import de.hsfl.budgetBinder.presentation.UiState
+import de.hsfl.budgetBinder.presentation.viewmodel.register.RegisterEvent
 import de.hsfl.budgetBinder.presentation.viewmodel.register.RegisterViewModel
 import di
+import kotlinx.coroutines.flow.collectLatest
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.*
 import org.kodein.di.instance
 
 
 @Composable
-fun RegisterComponent(screenState: MutableState<Screen>) {
-    /*al scope = CoroutineScope(Dispatchers.Unconfined + SupervisorJob())
-    val registerUseCase: RegisterUseCase by di.instance()
-    val loginUseCase: LoginUseCase by di.instance()
-    val getMyUserUseCase : GetMyUserUseCase by di.instance()
-    val viewModel = RegisterViewModel(registerUseCase,loginUseCase,getMyUserUseCase, scope)
-    val viewState = viewModel.state.collectAsState(scope)*/
-    val scope = rememberCoroutineScope()
+fun RegisterComponent() {
     val viewModel: RegisterViewModel by di.instance()
-    val viewState = viewModel.state.collectAsState(scope.coroutineContext)
+    val firstNameTextState = viewModel.firstNameText.collectAsState()
+    val lastNameTextState = viewModel.lastNameText.collectAsState()
+    val emailTextState = viewModel.emailText.collectAsState()
+    val passwordTextState = viewModel.passwordText.collectAsState()
+    val confirmedPasswordTextState = viewModel.confirmedPasswordText.collectAsState()
+    val loadingState = remember { mutableStateOf(false) }
 
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UiEvent.ShowLoading -> {
+                    // TODO: Refactor this, it's working but ahh
+                    loadingState.value = true
+                }
+                else -> loadingState.value = false
+            }
+        }
+    }
+    if (loadingState.value) {
+        Text("Loading")
+        //TODO: LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+    }
+    //Body
+    Header(
+        attrs = {
+            classes("mdc-top-app-bar")
+        }
+    ) {
+        Div(
+            attrs = {
+                classes("mdc-top-app-bar__row")
+            }
+        ) {
+            Section(
+                attrs = {
+                    classes("mdc-top-app-bar__section", "mdc-top-app-bar__section--align-start")
+                }
+            ) {
+                Img(
+                    src = "images/Logo.png", alt = "Logo", attrs = {
+                        classes("mdc-icon-button", AppStylesheet.image)
+                    }
+                )
+                Span(
+                    attrs = {
+                        classes("mdc-top-app-bar__title")
+                    }
+                ) {
+                    Text("Budget-Binder")
+                }
+            }
+            Section(
+                attrs = {
+                    classes("mdc-top-app-bar__section", "mdc-top-app-bar__section--align-end")
+                }
+            ) {
+                Button(
+                    attrs = {
+                        classes("mdc-button", "mdc-button--raised", "mdc-top-app-bar__navigation-icon")
+                        onClick { viewModel.onEvent(RegisterEvent.OnLoginScreen) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-button__label")
+                        }
+                    ) {
+                        Text("Login Instead")
+                    }
+                }
+            }
+        }
+    }
 
-    RegisterView(
-        state = viewState,
-        onRegisterButtonPressed = { firstName, lastName, email, password ->
-            viewModel._register(User.In(firstName,lastName,email,password))
-        },
-        onChangeToLogin = { screenState.value = Screen.Login }
-    )
+    MainFlexContainer {
+        // -- Register Form --
+        H1 { Text(" Register") }
+        Form(attrs = { //Probably possible with just a button OnClick instead of Form&Submit
+            this.addEventListener("submit") {
+                console.log("$firstNameTextState, $lastNameTextState, $emailTextState, $passwordTextState")
+                viewModel.onEvent(RegisterEvent.OnRegister)
+                it.preventDefault()
+            }
+        }
+        ) {
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Firstname") }
+                    Input(
+                        type = InputType.Text
+                    ) {
+                        classes("mdc-text-field__input")
+                        value(firstNameTextState.value.firstName)
+                        onInput {
+                            viewModel.onEvent(RegisterEvent.EnteredFirstname(it.value))
+                        }
+                    }
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Lastname") }
+                    Input(
+                        type = InputType.Text
+                    ) {
+                        classes("mdc-text-field__input")
+                        value(lastNameTextState.value.firstName)
+                        onInput {
+                            viewModel.onEvent(RegisterEvent.EnteredLastname(it.value))
+                        }
+                    }
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Email") }
+                    EmailInput(value = emailTextState.value.email,
+                        attrs = {
+                            classes("mdc-text-field__input")
+                            onInput {
+                                viewModel.onEvent(RegisterEvent.EnteredEmail(it.value))
+
+                            }
+                        })
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                Label(
+                    attrs = {
+                        classes("mdc-text-field", "mdc-text-field--filled")
+                        style { width(100.percent) }
+                    }
+                ) {
+                    Span(
+                        attrs = {
+                            classes("mdc-text-field__ripple")
+                        }
+                    ) { }
+                    Span(
+                        attrs = {
+                            classes("mdc-floating-label", "mdc-floating-label--float-above")
+                        }
+                    ) { Text("Password") }
+                    PasswordInput(value = passwordTextState.value.password,
+                        attrs = {
+                            classes("mdc-text-field__input")
+                            onInput {
+                                viewModel.onEvent(RegisterEvent.EnteredPassword(it.value))
+                            }
+                        })
+                    Span(
+                        attrs = {
+                            classes("mdc-line-ripple")
+                        }
+                    ) { }
+                }
+            }
+            Div(
+                attrs = {
+                    classes(AppStylesheet.margin)
+                }
+            ) {
+                SubmitInput(
+                    attrs = {
+                        classes("mdc-button", "mdc-button--raised")
+                        value("Submit")
+                    })
+            }
+        }
+    }
 }
