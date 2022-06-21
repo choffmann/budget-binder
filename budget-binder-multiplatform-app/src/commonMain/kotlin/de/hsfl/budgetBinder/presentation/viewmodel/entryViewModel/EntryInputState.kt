efeb604aa434a5b7ea6154e27aacc700ec4e6432
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-22 00:15:01 +0200

    Change data input variable structure to have a more precise type

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/entryViewModel/EntryInputState.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/entryViewModel/EntryInputState.kt
@@ -2,7 +2,7 @@ package de.hsfl.budgetBinder.presentation.viewmodel.entryViewModel
 
 data class EntryInputState(
     val name: String = "",
-    val amount: String = "",
+    val amount: Float = 0f,
     val repeat: Boolean = false,
     val categoryID: Int? = null,
     val amountSign: Boolean = false
