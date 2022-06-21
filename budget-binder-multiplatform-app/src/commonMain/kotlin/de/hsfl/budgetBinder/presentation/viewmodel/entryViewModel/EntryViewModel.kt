911bf79b6327a93caf5b049e107f116759fa7e2e
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-22 00:10:51 +0200

    Add button events, rename some data input variable names

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/entryViewModel/EntryViewModel.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/presentation/viewmodel/entryViewModel/EntryViewModel.kt
@@ -3,6 +3,7 @@ package de.hsfl.budgetBinder.presentation.viewmodel.entryViewModel
 import de.hsfl.budgetBinder.common.DataResponse
 import de.hsfl.budgetBinder.common.Entry
 import de.hsfl.budgetBinder.domain.usecase.*
+import de.hsfl.budgetBinder.presentation.Screen
 import de.hsfl.budgetBinder.presentation.UiState
 import de.hsfl.budgetBinder.presentation.flow.DataFlow
 import de.hsfl.budgetBinder.presentation.flow.RouterFlow
@@ -20,28 +21,30 @@ class EntryViewModel(
     private val scope: CoroutineScope
 ) {
     /* *** Variables *** */
+
+    // ---- Data Input Variables ----
     private val _nameText = MutableStateFlow(EntryInputState())
     val nameText: StateFlow<EntryInputState> = _nameText
 
     private val _amountText = MutableStateFlow(EntryInputState())
     val amountText: StateFlow<EntryInputState> = _amountText
 
-    private val _repeatText = MutableStateFlow(EntryInputState())
-    val repeatText: StateFlow<EntryInputState> = _repeatText
+    private val _repeatState = MutableStateFlow(EntryInputState())
+    val repeatState: StateFlow<EntryInputState> = _repeatState
 
-    private val _categoryIDText = MutableStateFlow(EntryInputState())
-    val categoryIDText: StateFlow<EntryInputState> = _categoryIDText
+    private val _categoryIDState = MutableStateFlow(EntryInputState())
+    val categoryIDState: StateFlow<EntryInputState> = _categoryIDState
 
-    private val _amountSignText = MutableStateFlow(EntryInputState())
-    val amountSignText: StateFlow<EntryInputState> = _amountSignText
+    private val _amountSignState = MutableStateFlow(EntryInputState())
+    val amountSignState: StateFlow<EntryInputState> = _amountSignState
 
 
-    //Default ViewModel Variables
+    // --- Default ViewModel Variables ----
     private val _dialogState = MutableStateFlow(false)
     val dialogState: StateFlow<Boolean> = _dialogState
     private val _eventFlow = UiEventSharedFlow.mutableEventFlow
     val eventFlow = UiEventSharedFlow.eventFlow
-
+    // ----
 
     init {
         //Do we need something to init?
@@ -55,11 +58,23 @@ class EntryViewModel(
             is EntryEvent.EnteredRepeat -> {}
             is EntryEvent.EnteredCategoryID -> {}
             is EntryEvent.EnteredAmountSign -> {}
-            is EntryEvent.OnCreateEntry -> {}
-            is EntryEvent.OnEditEntry -> {}
-            is EntryEvent.OnDeleteEntry -> {}
-            is EntryEvent.OnDeleteDialogConfirm -> {}
-            is EntryEvent.OnDeleteDialogDismiss -> {}
+            is EntryEvent.OnCreateEntry -> {
+                when (routerFlow.state.value) {
+                    is Screen.Entry.Create -> createEntry(
+                        Entry.In(
+                            nameText,
+                            amountState,
+                            repeatState,
+                            categoryIDState
+                        )
+                    )
+                    else -> routerFlow.navigateTo(Screen.Entry.Create)
+                }
+            }
+            is EntryEvent.OnEditEntry -> routerFlow.navigateTo(Screen.Settings.Server)
+            is EntryEvent.OnDeleteEntry -> _dialogState.value = true
+            is EntryEvent.OnDeleteDialogConfirm -> removeEntry()//TODO Entry ID
+            is EntryEvent.OnDeleteDialogDismiss -> _dialogState.value = false
             else -> {
                 throw Exception("Unhandled EntryEvent in EntryViewModel")
             }
