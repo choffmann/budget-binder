9e2a2e5f6f05cef8051e373f58acb5fb252f9c60
Fabian Petersen <fabian@nf-petersen.de>
2022-06-23 00:03:53 +0200

    restructured Config and ConfigIntermediate.kt
    we don't need configString anymore, we now pass a ConfigIntermediate
    to Config.create(ConfigIntermediate) and get a Config.

--- budget-binder-server/src/main/kotlin/de/hsfl/budgetBinder/server/config/Config.kt
+++ budget-binder-server/src/main/kotlin/de/hsfl/budgetBinder/server/config/Config.kt
@@ -1,5 +1,7 @@
 package de.hsfl.budgetBinder.server.config
 
+import java.io.File
+
 
 data class Config(val server: Server, val database: Database, val jwt: JWT) {
     data class Server(
@@ -39,4 +41,98 @@ data class Config(val server: Server, val database: Database, val jwt: JWT) {
         val issuer: String,
         val audience: String
     )
+
+    companion object {
+        fun create(configFile: File? = null): Config {
+            return configFile?.let {
+                createFromIntermediate(ConfigIntermediate.createFromFile(it))
+            } ?: createFromIntermediate(ConfigIntermediate.createFromEnv())
+        }
+
+        fun createFromIntermediate(intermediate: ConfigIntermediate): Config {
+            val dbType = intermediate.database.dbType
+
+            val sqlitePath: String
+            val dbServerAddress: String
+            val dbServerPort: String
+            val dbName: String
+            val dbUser: String
+            val dbPassword: String
+            if (dbType == DBType.SQLITE) {
+                sqlitePath = intermediate.database.sqlitePath ?: (System.getProperty("user.dir") + "/data/data.db")
+                dbServerAddress = ""
+                dbServerPort = ""
+                dbName = ""
+                dbUser = ""
+                dbPassword = ""
+            } else {
+                sqlitePath = ""
+                dbServerAddress = intermediate.database.serverAddress ?: error("No dbServerAddress specified")
+                dbServerPort = intermediate.database.serverPort ?: error("No dbServerPort specified")
+                dbName = intermediate.database.name ?: error("No dbDatabaseName specified")
+                dbUser = intermediate.database.user ?: error("No dbUser specified")
+                dbPassword = intermediate.database.password ?: error("No dbPassword specified")
+            }
+
+            val dev = intermediate.server?.dev ?: false
+            val ssl = intermediate.server?.ssl ?: false
+
+            val host = intermediate.server?.host ?: "0.0.0.0"
+            val port = intermediate.server?.port ?: 8080
+            val sslHost = intermediate.server?.sslHost ?: "0.0.0.0"
+            val sslPort = intermediate.server?.sslPort ?: 8443
+
+            val keyStorePassword: String
+            val keyStorePath: String
+            if (ssl) {
+                keyStorePassword = intermediate.server?.keyStorePassword
+                    ?: if (dev) "budget-binder-server" else error("No KeystorePassword provided")
+                keyStorePath = intermediate.server?.keyStorePath
+                    ?: if (dev) "data/dev_keystore.jks" else error("No KeystorePath provided")
+            } else {
+                keyStorePassword = ""
+                keyStorePath = ""
+            }
+
+            val forwardedHeaderSupport = !(intermediate.server?.noForwardedHeaderSupport ?: false)
+
+            val jwtAccessSecret = intermediate.jwt.accessSecret
+            val jwtRefreshSecret = intermediate.jwt.refreshSecret
+            val jwtAccessMinutes = intermediate.jwt.accessMinutes ?: 15
+            val jwtRefreshDays = intermediate.jwt.refreshDays ?: 7
+            val jwtRealm = intermediate.jwt.realm ?: "budget-binder-server"
+            val jwtIssuer = intermediate.jwt.issuer ?: "http://0.0.0.0:8080/"
+            val jwtAudience = intermediate.jwt.audience ?: "http://0.0.0.0:8080/"
+
+            return Config(
+                database = Database(
+                    dbType,
+                    sqlitePath,
+                    dbServerAddress,
+                    dbServerPort,
+                    dbName,
+                    dbUser,
+                    dbPassword
+                ), server = Server(
+                    dev,
+                    ssl,
+                    host,
+                    port,
+                    sslHost,
+                    sslPort,
+                    keyStorePassword,
+                    keyStorePath,
+                    forwardedHeaderSupport
+                ), jwt = JWT(
+                    jwtAccessSecret,
+                    jwtRefreshSecret,
+                    jwtAccessMinutes,
+                    jwtRefreshDays,
+                    jwtRealm,
+                    jwtIssuer,
+                    jwtAudience
+                )
+            )
+        }
+    }
 }
