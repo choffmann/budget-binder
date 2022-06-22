b0b9d873817e0fa57c2874b60e0e3155efea50a6
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-06-22 15:22:22 +0200

    Remove old data parts from web entrycomponent

--- budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entry/EntryComponent.kt
+++ budget-binder-multiplatform-app/src/jsMain/kotlin/de/hsfl/budgetBinder/compose/entry/EntryComponent.kt
@@ -3,15 +3,20 @@ package de.hsfl.budgetBinder.compose.entry
 import androidx.compose.runtime.*
 import de.hsfl.budgetBinder.common.Category
 import de.hsfl.budgetBinder.common.Entry
+import de.hsfl.budgetBinder.compose.MainFlexContainer
+import de.hsfl.budgetBinder.compose.NavBar
 import de.hsfl.budgetBinder.compose.category.categoryIdToCategory
 import de.hsfl.budgetBinder.compose.theme.AppStylesheet
 import de.hsfl.budgetBinder.domain.usecase.*
 import de.hsfl.budgetBinder.presentation.CategoryImageToIcon
 import de.hsfl.budgetBinder.presentation.Screen
+import de.hsfl.budgetBinder.presentation.UiEvent
+import de.hsfl.budgetBinder.presentation.flow.RouterFlow
 
 import de.hsfl.budgetBinder.presentation.viewmodel.dashboard.DashboardViewModel
 import de.hsfl.budgetBinder.presentation.viewmodel.entryViewModel.EntryViewModel
 import di
+import kotlinx.coroutines.flow.collectLatest
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 import org.kodein.di.instance
@@ -19,55 +24,59 @@ import kotlin.math.absoluteValue
 
 @Composable
 fun EntryComponent() {
+
     val viewModel: EntryViewModel by di.instance()
+    val routerFlow: RouterFlow by di.instance()
+    val screenState = routerFlow.state.collectAsState()
+    val loadingState = remember { mutableStateOf(false) }
     //Data to load
     val entry = viewModel.selectedEntryState.collectAsState()
-    val loadingState = remember { mutableStateOf(false) }
+    val categoryList = viewModel.categoryListState.collectAsState()
 
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is UiEvent.ShowLoading -> loadingState.value = true
+                is UiEvent.HideSuccess -> loadingState.value = false
+                else -> loadingState.value = false
+            }
+        }
+    }
+    NavBar {}
+    MainFlexContainer {
+        when (screenState.value) {
+            is Screen.EntryCreate -> {
+                EntryCreateView(
+                    categoryList = (screenState.value as Screen.EntryCreate).categoryList,
+                    onCreateEntryButtonPressed = { name: String, amount: Float, repeat: Boolean, category_id: Int ->
+                        viewModel.createEntry(Entry.In(name, amount, repeat, category_id))
+                    }
+                )
 
-    when (screenState.value) {
-        is Screen.EntryCreate -> {
-            EntryCreateView(
-                state = viewState,
-                categoryList = (screenState.value as Screen.EntryCreate).categoryList,
-                onChangeToDashboard = { screenState.value = Screen.Dashboard },
-                onCreateEntryButtonPressed = { name:String, amount:Float, repeat:Boolean, category_id:Int ->
-                    viewModel.createEntry(Entry.In(name, amount, repeat, category_id))
-                },
-                onChangeToSettings = { screenState.value = Screen._Settings },
-                onChangeToCategory = { screenState.value = Screen.CategorySummary },
-            )
-
+            }
+            is Screen.EntryEdit -> {
+                EntryEditView(
+                    onEditEntryButtonPressed = { name: String, amount: Float, repeat: Boolean, category: Entry.Category? ->
+                        viewModel.changeEntry(
+                            Entry.Patch(name, amount, repeat, category),
+                            (screenState.value as Screen.EntryEdit).id
+                        )
+                    }
+                )
+                viewModel.getEntryById((screenState.value as Screen.EntryEdit).id)//(screenState.value as Screen.EntryEdit).id)
+            }
+            is Screen.EntryOverview -> {
+                EntryOverviewView(
+                    onEditButton = { id, -> screenState.value = Screen.EntryEdit(id) },
+                    onDeleteButton = { id ->
+                        viewModel.removeEntry(id)
+                        screenState.value = Screen.Dashboard
+                    }
+                )
+                viewModel.getEntryById((screenState.value as Screen.EntryOverview).id)
+            }
+            else -> {}
         }
-        is Screen.EntryEdit -> {
-            EntryEditView(
-                state = viewState,
-                onChangeToDashboard = { screenState.value = Screen.Dashboard },
-                onEditEntryButtonPressed = { name:String, amount:Float, repeat:Boolean, category:Entry.Category? ->
-                    viewModel.changeEntry(
-                        Entry.Patch(name, amount, repeat, category),
-                        (screenState.value as Screen.EntryEdit).id
-                    )
-                },
-                onChangeToSettings = { screenState.value = Screen._Settings },
-                onChangeToCategory = { screenState.value = Screen.CategorySummary },
-            )
-            viewModel.getEntryById((screenState.value as Screen.EntryEdit).id)//(screenState.value as Screen.EntryEdit).id)
-        }
-        is Screen.EntryOverview -> {
-            EntryOverviewView(
-                state = viewState,
-                onEditButton = {id, -> screenState.value = Screen.EntryEdit(id)},
-                onDeleteButton = { id ->
-                    viewModel.removeEntry(id)
-                    screenState.value = Screen.Dashboard},
-                onChangeToDashboard = { screenState.value = Screen.Dashboard },
-                onChangeToSettings = { screenState.value = Screen._Settings },
-                onChangeToCategory = { screenState.value = Screen.CategorySummary },
-            )
-            viewModel.getEntryById((screenState.value as Screen.EntryOverview).id)
-        }
-        else -> {}
     }
 }
 
