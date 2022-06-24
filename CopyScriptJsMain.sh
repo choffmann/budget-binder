5628efd08abc84e45bf068945a3f9959daca883f
Fabian Petersen <fabian@nf-petersen.de>
2022-06-24 11:53:27 +0200

    updated the script file so that it works on linux

--- CopyScriptJsMain.sh
+++ CopyScriptJsMain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp -r "./budget-binder-multiplatform-app/build/distributions/*" "./budget-binder-server/public/"
+cp -r ./budget-binder-multiplatform-app/build/distributions/* ./budget-binder-server/public/
