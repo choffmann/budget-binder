fb459a7f63ae8813ccf1bdd797f68901911f744e
Fabian Petersen <fabian@nf-petersen.de>
2022-06-25 21:59:27 +0200

    make SettingsRepositoryImpl expect actual because the Web-Client doesn't support these settings.

--- budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/data/repository/SettingsRepositoryImpl.kt
+++ budget-binder-multiplatform-app/src/commonMain/kotlin/de/hsfl/budgetBinder/data/repository/SettingsRepositoryImpl.kt
@@ -1,36 +1,13 @@
 package de.hsfl.budgetBinder.data.repository
 
-import com.russhwolf.settings.*
 import de.hsfl.budgetBinder.domain.repository.SettingsRepository
 
-object SettingsRepositoryImpl: SettingsRepository {
-    private val settings: Settings = Settings()
-
-    override fun storeFirstTimeUse() {
-        settings.putBoolean("after_first_time_use", true)
-    }
-
-    override fun checkIsFirstTime(): Boolean {
-        return !settings.hasKey("after_first_time_use")
-    }
-
-    override fun storeDarkTheme(isDarkMode: Boolean) {
-        settings.putBoolean("is_dark_mode", isDarkMode)
-    }
-
-    override fun getDarkMode(): Boolean {
-        return settings.getBoolean("is_dark_mode")
-    }
-
-    override fun storeServerUrl(url: String) {
-        settings.putString("server_url", url)
-    }
-
-    override fun checkHasServerUrlKey(): Boolean {
-        return settings.hasKey("server_url")
-    }
-
-    override fun getServerUrl(): String {
-        return settings.getString("server_url", defaultValue = "http://localhost")
-    }
+expect object SettingsRepositoryImpl : SettingsRepository {
+    override fun storeFirstTimeUse()
+    override fun checkIsFirstTime(): Boolean
+    override fun storeDarkTheme(isDarkMode: Boolean)
+    override fun getDarkMode(): Boolean
+    override fun storeServerUrl(url: String)
+    override fun checkHasServerUrlKey(): Boolean
+    override fun getServerUrl(): String
 }
